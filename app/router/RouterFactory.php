fc95d32b88d8ec26d97b766d0445f06719bb0ee6
Tomáš Blatný <blatny.tomas@seznam.cz>
2014-10-03 01:34:48 +0200

    Routing

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -56,29 +56,29 @@ class RouterFactory
 
 	protected function createProductionRouter($router)
 	{
-		$router[] = new Route('//blog.blatny.org/compose', array(
+		$router[] = new Route('//blog.%domain%/compose', array(
 			'module' => 'Blog',
 			'presenter' => 'Article',
 			'action' => 'compose',
 		));
-		$router[] = new Route('//blog.blatny.org/<slug>[/<action>]', array(
+		$router[] = new Route('//blog.%domain%/<slug>[/<action>]', array(
 			'module' => 'Blog',
 			'presenter' => 'Article',
 			'action' => 'detail',
 			'slug' => NULL,
 		));
-		$router[] = new Route('//projects.blatny.org/new', array(
+		$router[] = new Route('//projects.%domain%/new', array(
 			'module' => 'Projects',
 			'presenter' => 'Project',
 			'action' => 'new',
 		));
-		$router[] = new Route('//projects.blatny.org/<slug>[/<action>[/<id>]]', array(
+		$router[] = new Route('//projects.%domain%/<slug>[/<action>[/<id>]]', array(
 			'module' => 'Projects',
 			'presenter' => 'Project',
 			'action' => 'detail',
 			'slug' => NULL,
 		));
-		$router[] = new Route('//[<module>.]blatny.org/<presenter>/<action>[/<id>]', array(
+		$router[] = new Route('//[<module>.]%domain%/<presenter>/<action>[/<id>]', array(
 			'module' => 'Public',
 			'presenter' => 'Dashboard',
 			'action' => 'default',
